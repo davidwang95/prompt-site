2ce3ac3c5869d74d5643131cea9aaf22718b2493
davidwang95 <wangwangdavid@gmail.com>
2025-03-27 22:45:52 -0400

    Simplify Analytics to match Google's code exactly

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,57 +1,23 @@
 'use client';
 
-import { usePathname, useSearchParams } from 'next/navigation';
 import Script from 'next/script';
-import { useEffect, Suspense } from 'react';
-
-// Replace this with your actual GA4 Measurement ID
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
-
-function AnalyticsContent() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    if (pathname && GA_MEASUREMENT_ID) {
-      // Push the new page view to GA4
-      window.gtag('config', GA_MEASUREMENT_ID, {
-        page_path: pathname + searchParams.toString(),
-      });
-    }
-  }, [pathname, searchParams]);
-
-  if (!GA_MEASUREMENT_ID) {
-    return null;
-  }
-
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-    </>
-  );
-}
 
 export default function Analytics() {
   return (
-    <Suspense>
-      <AnalyticsContent />
-    </Suspense>
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-W87N87MDDZ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-W87N87MDDZ');
+        `}
+      </Script>
+    </>
   );
 } 
